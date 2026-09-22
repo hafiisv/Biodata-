2d62743a4525f8b7c59305118a1d614245ba9570
Hafis <hafiismiu@gmail.com>
2026-09-22 19:48:45 +0700

    Create Tugas biodata.java

---
+++ Tugas biodata.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Nama :Muhammad hafis");
+		System.out.println("asal :desa.kujung,kecamatan.Widang,kab.tuban");
+		System.out.println("Nim :261301001");
+		System.out.println("jurusan : Sistem komputer");
+		System.out.println("universitas : universitas Nahdhatul ulama");
+	}
+}
